f3c62949b10d5e0106e53ae24dc33a4b3fdaccc0
REINBOLD Vincent <vincent.reinbold@supelec.fr>
2019-10-11 14:23:50 +0200

    adding microgrid and modules for making an example

---
+++ microgrid/batteries.py
@@ -0,0 +1,151 @@
+from pyomo.environ import *
+from pyomo.dae import *
+from pyomo.network import Port
+
+UB = 1e6
+
+
+class Unit(SimpleBlock):
+    """
+    redefinition of SimpleBlock
+    """
+
+    def __init__(self, *args, **kwds):
+        """
+
+        :param args:
+        :param kwds:
+        """
+        super().__init__(*args, **kwds)
+        logger.info(f'Initiation of {self.name}...')
+
+
+class AbsDynUnit(Unit):
+    def __init__(self, *args, **kwds):
+        """
+        Abstract Dynamic Unit
+
+        Standard unit with time attribute to index variables and constraints.
+
+        :param args:
+        :param time:
+        :param kwds:
+        """
+        from pyomo.dae.contset import ContinuousSet
+
+        super().__init__(*args, **kwds)
+        self.time = ContinuousSet()
+        self.doc = self.__doc__
+
+    def pplot(self, **kwargs):
+
+        """
+        Plotting method for AbsDyn Unit. It iterates on the component ctype of the Block and plots it.
+        :param kwargs:
+        :return: lines, axe and figure
+        """
+        from .utils import pplot
+
+        ctype = kwargs.pop('ctype', Var)
+        active = kwargs.pop('active', True)
+        lines, ax, fig = pplot(*[v for v in self.component_objects(ctype=ctype, active=active)], **kwargs)
+        return lines, ax, fig
+
+
+class AbsBatteryV0(AbsDynUnit):
+    """
+    Battery with ideal efficiency.
+
+    This battery is limited in power, variation of power and energy. One can fix initial and final stored energy.
+    For fixing initial en final state of charge, consider using `AbsBatteryV1`.
+    It exposes one power port using source convention.
+
+
+    """
+
+    def __init__(self, *args, **kwds):
+
+        super().__init__(*args, **kwds)
+
+        self.p      = Var(self.time, doc='energy derivative with respect to time',  initialize=0)
+        self.e      = Var(self.time, doc='energy in battery',                       initialize=0)
+
+        self.emin   = Param(default=0,       doc='minimum energy (kWh)',       mutable=True, within=NonNegativeReals)
+        self.emax   = Param(default=UB,      doc='maximal energy',             mutable=True)
+        self.e0     = Param(default=None,    doc='initial state',              mutable=True)
+        self.ef     = Param(default=None,    doc='final state',                mutable=True)
+        self.etac   = Param(default=1.0,     doc='charging efficiency',        mutable=True)
+        self.etad   = Param(default=1.0,     doc='discharging efficiency',     mutable=True)
+        self.dpdmax = Param(default=UB,      doc='maximal discharging power',  mutable=True)
+        self.dpcmax = Param(default=UB,      doc='maximal charging power',     mutable=True)
+
+        self.pcmax  = Param(default=UB,      doc='maximal charging power',     mutable=True, within=PositiveReals)
+        self.pdmax  = Param(default=UB,      doc='maximal discharging power',  mutable=True, within=PositiveReals)
+
+        self.de     = DerivativeVar(self.e, wrt=self.time, initialize=0,
+                                    doc='variation of energy  with respect to time')
+        self.dp     = DerivativeVar(self.p, wrt=self.time, initialize=0,
+                                    doc='variation of the battery power with respect to time',
+                                    bounds=lambda m, t: (-m.dpcmax, m.dpdmax))
+
+        self.outlet = Port(initialize={'f': (self.p, Port.Conservative)})
+
+        def _p_init(m, t):
+            if t == m.time.bounds()[0]:
+                return m.p[t] == 0
+            return Constraint.Skip
+
+        def _e_initial(m, t):
+            if m.e0.value is not None:
+                if t == 0:
+                    return m.e[t] == m.e0
+            return Constraint.Skip
+
+        def _e_final(m, t):
+            if m.ef.value is None:
+                return Constraint.Skip
+            if t == m.time.last():
+                return m.ef-1e-5, m.e[t], m.ef+1e-5
+            else:
+                return Constraint.Skip
+
+        def _e_min(m, t):
+            if m.emin.value is None:
+                return Constraint.Skip
+            return m.e[t] >= m.emin
+
+        def _e_max(m, t):
+            if m.emax.value is None:
+                return Constraint.Skip
+            return m.e[t] <= m.emax
+
+        def _pmax(m, t):
+            if m.pcmax.value is None:
+                return Constraint.Skip
+            else:
+                return -m.pcmax, m.p[t], m.pdmax
+
+        def _dpcmax(m, t):
+            if m.dpcmax.value is None:
+                return Constraint.Skip
+            else:
+                return m.dp[t] >= -m.dpcmax
+
+        def _dpdmax(m, t):
+            if m.dpdmax.value is None:
+                return Constraint.Skip
+            else:
+                return m.dp[t] <= m.dpdmax
+
+        def _energy_balance(m, t):
+            return m.de[t] == 1/3600*(m.p[t])
+
+        self._e_balance = Constraint(self.time, rule=_energy_balance, doc='Energy balance constraint')
+        self._p_init    = Constraint(self.time, rule=_p_init,    doc='Initialize power')
+        self._e_initial = Constraint(self.time, rule=_e_initial, doc='Initial energy constraint')
+        self._e_final   = Constraint(self.time, rule=_e_final,   doc='Final stored energy constraint')
+        self._e_min     = Constraint(self.time, rule=_e_min,     doc='Minimal energy constraint')
+        self._e_max     = Constraint(self.time, rule=_e_max,     doc='Maximal energy constraint')
+        self._pmax      = Constraint(self.time, rule=_pmax,      doc='Power bounds constraint')
+        self._dpdmax    = Constraint(self.time, rule=_dpdmax,    doc='Maximal varation of descharging power constraint')
+        self._dpcmax    = Constraint(self.time, rule=_dpcmax,    doc='Maximal varation of charging power constraint')
---
+++ microgrid/maingrids.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Distribution Grid Units
+
+Electrical connection to the distribution grid.
+
+"""
+from .base_units import AbsDynUnit, AbsPowerSource, def_bilinear_cost, def_linear_cost, def_bilinear_dynamic_cost
+from .cost import def_bilinear_cost, def_bilinear_dynamic_cost
+
+from pyomo.environ import Constraint,  Var, Param, Block, Expression, Piecewise
+from pyomo.network import Port
+from pyomo.environ import NonNegativeReals, Binary
+from pandas import Series
+
+__all__ = ['AbsMainGridV0', 'AbsMainGridV1']
+
+UB = 10e6
+
+
+def def_pin_pout(m):
+    # This could be better implemented.
+    # In particular, it should not use m.p directly but m.find_component(name_var)
+
+    """
+    Defines power 'in' and 'out' variables
+
+    :param m:
+    :return:
+    """
+
+    assert isinstance(m, Block),    f"argument 'm', must be an instance of Block, but is actually {type(m)}."
+    assert hasattr(m, 'p'),         f"model m does not have attribute named 'p'. This is needed. "
+
+    m.pmax  = Param(initialize=UB,  mutable=True, doc='maximal power out (kW)')
+    m.pmin  = Param(initialize=UB,  mutable=True, doc='maximal power in (kW)')
+
+    m.pout  = Var(m.time, doc='power to the main grid',   within=NonNegativeReals,    initialize=None)
+    m.pin   = Var(m.time, doc='power from the main grid', within=NonNegativeReals,    initialize=None)
+    m.u     = Var(m.time, doc='binary variable',          within=Binary,              initialize=None)
+
+    def _power_balance(b, t):
+        return b.p[t] - b.pout[t] + b.pin[t] == 0
+
+    def _pmax(b, t):
+        if b.pmax.value is None:
+            return Constraint.Skip
+        return b.pout[t] - b.u[t] * b.pmax <= 0
+
+    def _pmin(b, t):
+        if b.pmin.value is None:
+            return Constraint.Skip
+        return b.pin[t] + b.u[t] * b.pmin <= b.pmin
+
+    m._pmin      = Constraint(m.time, rule=_pmin,          doc='low bound')
+    m._pmax      = Constraint(m.time, rule=_pmax,          doc='up bound')
+    m._p_balance = Constraint(m.time, rule=_power_balance, doc='power balance')
+
+
+class AbsMainGridV0(AbsPowerSource):
+    """
+    Simple MainGrid Unit.
+
+    One Power port (named 'p' by default) associated with a simple cost (named 'cost').
+    (Source convention)
+    """
+    def __init__(self, *args, flow_name='p', **kwgs):
+
+        super().__init__(*args, flow_name=flow_name, **kwgs)
+        self.instant_cost = def_linear_cost(self, var_name=flow_name)
+        self.component(flow_name).doc = 'Supplied power from the maingrid (source convention)'
+
+
+class AbsMainGridV1(AbsPowerSource):
+    """
+    Main Grid Unit.
+
+    Consists of a power source with limits (pmin, pmax),
+    associated with a bilinear cost (selling and buying cost, i.e. c_in and c_out).
+    A binary variable 'u' is declared to tackle the price discontinuity at p=0.
+    (Source convention)
+
+    """
+    def __init__(self, *args, flow_name='p', **kwgs):
+        super().__init__(*args, flow_name=flow_name, **kwgs)
+        def_pin_pout(self)
+        self.instant_cost = def_bilinear_cost(self, var_in='pin', var_out='pout')
